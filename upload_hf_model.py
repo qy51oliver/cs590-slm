e6f8c29f512df16ab9ec7cdbb4d6bdd87339f678
Oliveryql <49108241+Oliveryql@users.noreply.github.com>
2025-11-23 15:20:26 -0500

    add model upload script

---
+++ upload_hf_model.py
@@ -0,0 +1,14 @@
+from huggingface_hub import HfApi
+
+api = HfApi()
+repo_id = "oliveryql/gemma270m-sft-v1" # change version number
+api.create_repo(repo_id, repo_type="model", private=True, exist_ok=True)
+
+api.upload_folder(
+    folder_path="~/cs590llm/cs590-slm/models/gemma270m-sft-v1",  # change model version here
+    repo_id=repo_id,
+    repo_type="model",
+    path_in_repo=".",                # upload at repo root
+    commit_message="Add SFT checkpoints",
+)
+print("Pushed to:", f"https://huggingface.co/{repo_id}")
