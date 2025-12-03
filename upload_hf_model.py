e15c9c01ade3ce12fc57170e65fd8136641b960f
Oliveryql <49108241+Oliveryql@users.noreply.github.com>
2025-12-03 16:39:20 -0500

    upload it-v1 model

--- upload_hf_model.py
+++ upload_hf_model.py
@@ -1,11 +1,11 @@
 from huggingface_hub import HfApi
 
 api = HfApi()
-repo_id = "oliveryql/gemma270m-sft-arc" # change version number
+repo_id = "oliveryql/gemma270-it-v1" # change version number
 api.create_repo(repo_id, repo_type="model", private=True, exist_ok=True)
 
 api.upload_folder(
-    folder_path="~/cs590llm/cs590-slm/models/gemma270m-sft-arc",  # change model version here
+    folder_path="~/cs590llm/cs590-slm/models/gemma270m-it-v1",  # change model version here
     repo_id=repo_id,
     repo_type="model",
     path_in_repo=".",                # upload at repo root
