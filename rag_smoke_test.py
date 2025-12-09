a70c324d14965df1fe763d772ca95a9e98d52251
Oliveryql <49108241+Oliveryql@users.noreply.github.com>
2025-12-09 16:07:58 -0500

    rag smoke test update

--- rag_smoke_test.py
+++ rag_smoke_test.py
@@ -3,8 +3,8 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 from pipelines import apply_instruct_template
 
 QUESTION = """When was the battle of Hastings?"""
-CONTEXT = """Alexander Fleming discovered penicillin in 1928 at St Mary's Hospital in London."""
-CONTEXT = ""
+CONTEXT = """“The Battle of Hastings was fought on 14 October 1066 between the Norman-French army of William, Duke of Normandy, and an English army under the Anglo-Saxon King Harold Godwinson, beginning the Norman Conquest of England. It took place approximately 7 mi (11 km) northwest of Hastings, close to the present-day town of Battle, East Sussex, and was a decisive Norman victory. The background to the battle was the death of the childless King Edward the Confessor in January 1066, which set up a succession struggle between several claimants to his throne. Harold was crowned king shortly after Edward's death but faced invasions by William, his own brother Tostig, and the Norwegian king Harald Hardrada (Harold III of Norway). Hardrada and Tostig defeated a hastily gathered army of Englishmen at the Battle of Fulford on 20 September 1066. They were in turn defeated by Harold at the Battle of Stamford Bridge on 25 September. The deaths of Tostig and Hardrada at Stamford Bridge left William as Harold's only serious opponent. While Harold and his forces were recovering, William landed his invasion forces in the south of England at Pevensey on 28 September and established a beachhead for his conquest...""""
+# CONTEXT = ""
 
 # -----------------------------------------
 
